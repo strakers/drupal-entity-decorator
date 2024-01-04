1ae539b136c5e9ee2f9c5719df2776b70916d3de
strakers <s.straker@utoronto.ca>
2024-01-04 16:33:57 -0500

    Update webform decorator class

--- src/Decorators/Webform/WebformDecorator.php
+++ src/Decorators/Webform/WebformDecorator.php
@@ -2,10 +2,17 @@
 
 namespace Drupal\entity_decorator\Decorators\Webform;
 
+use Drupal\entity_decorator\Traits\IsUserOwned;
+
 class WebformDecorator extends \Drupal\entity_decorator\Base\ContentEntityDecoratorBase {
+  use IsUserOwned;
 
   protected static function getClassOrModelName(): string {
     return 'Drupal\webform\Entity\Webform';
+  }
+
+  public function getTitle(): string {
+    return $this->getFieldData('title', '');
   }
 
 }

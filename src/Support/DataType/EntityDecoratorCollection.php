8316cd2e35115b7e2cb92702ed05f0ab60e83520
strakers <s.straker@utoronto.ca>
2024-01-12 12:45:28 -0500

    Use decorator pattern to override collection

--- src/Support/DataType/EntityDecoratorCollection.php
+++ src/Support/DataType/EntityDecoratorCollection.php
@@ -5,10 +5,25 @@ namespace Drupal\entity_decorator\Support\DataType;
 use loophp\collection\Collection;
 use loophp\collection\Contract\Collection as CollectionInterface;
 
-class EntityDecoratorCollection extends Collection {
-  public function values(): CollectionInterface {
-    $arr = $this->all();
-    return static::fromIterable(array_values($arr));
+class EntityDecoratorCollection {
+
+  protected CollectionInterface $collection;
+
+  public static function fromIterable(iterable $iterable): static {
+    return new static($iterable);
+  }
+
+  public function __construct(iterable $iterable) {
+    if ($iterable instanceof CollectionInterface) {
+      $this->collection = $iterable;
+    }
+    else {
+      $this->collection = Collection::fromIterable($iterable)->strict();
+    }
+  }
+
+  public function __call($name, $arguments) {
+    return $this->{$name}(...$arguments);
   }
 
   public function search(callable ...$callbacks) {

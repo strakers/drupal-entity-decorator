33cb43b10e0f4f1abb616e0545b2dfd27f66b900
strakers <s.straker@utoronto.ca>
2024-01-12 13:08:01 -0500

    Update collection decorator definition

--- src/Support/DataType/EntityDecoratorCollection.php
+++ src/Support/DataType/EntityDecoratorCollection.php
@@ -3,6 +3,7 @@
 namespace Drupal\entity_decorator\Support\DataType;
 
 use loophp\collection\Collection;
+use loophp\collection\Operation\Filter;
 use loophp\collection\Contract\Collection as CollectionInterface;
 
 class EntityDecoratorCollection {
@@ -23,10 +24,26 @@ class EntityDecoratorCollection {
   }
 
   public function __call($name, $arguments) {
-    return $this->{$name}(...$arguments);
+    if (method_exists($this->collection, $name)) {
+      if (in_array($name, ['all', 'current', 'first', 'last'])) {
+        return $this->collection->{$name}(...$arguments);
+      }
+      else {
+        $this->collection = $this->collection->{$name}(...$arguments);
+        return $this;
+      }
+    }
+    return null;
   }
 
   public function search(callable ...$callbacks) {
-    return $this->filter($callbacks)->first()->current(0, null);
+    $this->collection = Collection::fromCallable((new Filter())()(...$callbacks), [$this->collection]);
+    return $this->collection->first()->current(0, null);
+  }
+
+  public function values(): static {
+    $arr = $this->collection->all();
+    $this->collection = Collection::fromIterable(array_values($arr))->strict();
+    return $this;
   }
 }
